202cb34cb9606b0812d79ece9bd43c75a1efb9f6
ilsa23 <ilze.sarai@gmail.com>
2015-03-07 15:34:41 -0600

    qwert

--- js/actions.js
+++ js/actions.js
@@ -1,25 +1,22 @@
 var fn = {
     ready: function(){
-        document.addEventListener("deviceready", fn.init, false);
+        document.addEventListener('deviceready',fn.init,false);
     },
-    
     init: function(){
         var x = false;
         if(!x)
             window.location.href = "#registro";
         $('#registro a:eq(1)').tap(fn.registrar);
-    }, 
+    },
     registrar: function(){
-       var nom = $('#registro input:eq(0)').val();
+        var nom = $('#registro input:eq(0)').val();
+        var mail = $('#registro input:eq(1)').val();
+        var tel = $('#registro input:eq(2)').val();
         
-       var mail = $('#registro input:eq(1)').val();
-       var tel = $('#registro input:eq(2)').val();
-    
-        if( nom != '' && mail != '' && tel != '')
-            alert(nom + '-' + mail + '-' + tel );
+        if(nom != '' && mail != '' && tel != '')
+            alert(nom + ' - ' + mail + ' - ' + tel);
         else
-            navigator.notification.alert('Todos los campos son requeridos',null,'Error de Datos','Aceptar');
+            navigator.notification.alert('Todos los campos son requeridos', null, 'Error de Datos','Aceptar');
     }
-    
 };
 $(fn.ready);
