9811beab47d8cdf5be2a0f54d12fbe7532cd90cc
ilsa23 <ilze.sarai@gmail.com>
2015-03-14 14:13:35 -0600

    D
    d

--- js/storage.js
+++ js/storage.js
@@ -3,7 +3,6 @@ var almacenamiento = {
     createReg: function(nom, id){
         window.localStorage.setItem('name',nom);
         window.localStorage.setItem('uuid',id);
-        
     },
     isRegistered: function(){
         if(window.localStorage.getItem('uuid') != undefined && window.localStorage.getItem('uuid') != '')
