6235e3152eda64b7c587d7c1e1fbd64ffdaf6375
ilsa23 <ilze.sarai@gmail.com>
2015-03-21 12:35:40 -0600

    ada

--- js/actions.js
+++ js/actions.js
@@ -7,6 +7,7 @@ var fn = {
             window.location.href = "#registro";
         $('#registro a:eq(0)').tap(capture.takePhoto);
         $('#registro a:eq(1)').tap(fn.registrar);
+        fn.crearReserva();
     },
     registrar: function(){
         var nom = $('#registro input:eq(0)').val();
@@ -18,6 +19,40 @@ var fn = {
             server.sendData(nom,mail,tel,img);
         else
             navigator.notification.alert('Todos los campos son requeridos', null, 'Error de Datos','Aceptar');
+    },
+    crearReserva: function()
+    {
+        var reserva = {
+            selecTH: function(){
+               if($(this).index() >0){
+               $('#nr1').attr('th',$(this).index());
+               $('#nr1 ul:eq(0) li a').css('background','#f6f6f6');
+               $(this).find('a').css('background','#00dd00');
+               }
+            },
+                siguiente: function()
+            {   var th = $('#nr1').attr('th');
+                if(th != undefined && th != '')
+                window.location.href= '#nr2';
+            },
+                reservar: function()
+            {   var th = $('#nr1').attr('th');
+                var ha = $('#nr2 select:eq(0)').val();
+                var pr = $('#nr2 select:eq(1)').val();
+                var di = $('#nr2 select:eq(2)').val();
+                if(connection.isConnected())
+                alert(th + ' - ' + ha + ' - ' + pr + ' - ' + di);
+             else
+                 almacenamiento.reservar(th,ha,pr,di);
+            }
+            
+        
+        };
+        $('#nr1 ul:eq(0) li').tap(reserva.selecTH);
+        $('#nr1 ul:eq(1) li:eq(1)').tap(reserva.siguiente);
+        $('#nr2 ul:eq(1) li:eq(1)').tap(reserva.reservar);
+        $('#historial').tap(almacenamiento.leerReservas);
+        
     }
 };
 $(fn.ready);
--- js/storage.js
+++ js/storage.js
@@ -9,5 +9,42 @@ var almacenamiento = {
             return true;
         else
             return false;
+    },
+    db: window.openDatabase("hotelApp","1.0","HotelApp",200000)
+    reservar: function(th,ha,pr,di){
+        
+function populateDB(tx) {
+        tx.executeSql('CREATE TABLE IF NOT EXISTS RESERVAS (id,th,ha,pr,di)');
+    tx.executeSql('INSERT INTO RESERVAS (th,ha,pr,di) VALUES ("'+th+'","'+ha+'","'+pr+'","'+di+'")');
+}
+
+function errorCB(err) {
+    alert("Error processing SQL: "+err);
+}
+
+function successCB() {
+   window.location.href = "#home";
+    navigator.notification.alert('Reserva guardada en espera de sincronización',null,'Guardado','Aceptar')
+}
+
+almacenamiento.db.transaction(populateDB, errorCB, successCB);
+},
+    leerReservas: function()
+{
+    function populateDB(tx)
+    {
+        tx.executeSql("SELECT * FROM reservas",[],function(tx2,r){
+            alert(r.rows.lenght);}, function (err){alert('Error: ' +err.code);});
     }
+    
+    function errorCB(err)
+    {
+        alert('Error: '+err.code);
+    }
+    function succesCB(){
+        var x = null;
+    }
+    
+    almacenamiento.db.transaction(populateDB,errorCB,succesCB);
+}
 };
